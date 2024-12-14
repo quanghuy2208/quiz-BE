c5c5f18c777a2e3e1b86d21c9fb1d0d8ffaec027
quanghuy2208 <2208huyquang@gmail.com>
2024-12-14 11:56:49 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3002;
+console.log("🚀 ~ port:", port);
+
+app.use(cors());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+routes(app);
+
+mongoose
+  .connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log("Connect Db success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("OK");
+  });
+app.listen(port, () => {
+  console.log("Server is running in port: ", +port);
+});
