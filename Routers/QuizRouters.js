0d17f76d09787b7f4286d36a9e5a5925b84955ac
quanghuy2208 <2208huyquang@gmail.com>
2024-12-16 08:50:00 +0700

    h

--- Routers/QuizRouters.js
+++ Routers/QuizRouters.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Category_quizControllers = require("../Controllers/Category_quizControllers");
+const Category_quizControllers = require("../Controllers/QuizControllers");
 const {
   authMiddleWare,
   authUserMiddleWare,
--- Routers/index.js
+++ Routers/index.js
@@ -1,7 +1,7 @@
 const QuizRouters = require("./QuizRouters");
 
 const routes = (app) => {
-  // app.use("/api/createCategory_quiz", QuizRouters);
+  app.use("/api/createCategory_quiz", QuizRouters);
 }
 
 module.exports = routes;
